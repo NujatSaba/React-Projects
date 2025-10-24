467c6ec514dcb4efd45dccc2a9e28fa6c573bee2
Nujat Saba <sabanujat14@gmail.com>
2025-10-24 18:35:40 +0600

    css done

--- user-card-ui/src/App.jsx
+++ user-card-ui/src/App.jsx
@@ -1,39 +1,43 @@
-import React from 'react'
-import {Bookmark} from 'lucide-react'
-
+import React from "react";
+import { Bookmark } from "lucide-react";
 
 const App = () => {
   return (
-    <div className='parent'>
+    <div className="parent">
       <div className="card">
         <div className="top">
-          <div className="logo">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png" alt="Google" />
-
-            <button>Save <Bookmark /></button>
-          </div>
+          <img className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"
+            alt="Google"
+          />
+          <button >
+            Save <Bookmark size='12px'/>
+          </button>
         </div>
 
         <div className="center">
-          <h2 className='user'>Airbnb <span>5 day ago</span></h2>
-          <h1 className='post'>Junior UI/UX Designer</h1>
+          <h2 className="user">
+            Airbnb <span>5 day ago</span>
+          </h2>
+          <h1 className="post">Junior UI/UX Designer</h1>
           <div className="tag">
             <h3>Contact</h3>
             <h3>Remote</h3>
           </div>
         </div>
 
+        <div className="line"></div>
+        
         <div className="bottom">
-          <div>  
-            <h1>$100/hr</h1>
-            <h4>Delhi, India</h4>
+          <div>
+            <h4 className="payment">$100/hr</h4>
+            <h6 className="location">Dhaka, Bangladesh</h6>
           </div>
           <button>Apply now</button>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
